b052110c39221a33f7bb14debb8e1f2cbcebb61d
Aviatus <aenes45@gmail.com>
2018-06-26 21:52:40 +0300

    training 4 5 6...

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+import java.util.RandomAccess;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton= (Button)findViewById(R.id.rollButton);
+        final ImageView main = (ImageView)findViewById(R.id.image_8balls);
+
+        final int[] randArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random randomGenerator = new Random();
+
+                int number1 = randomGenerator.nextInt(5);
+                main.setImageResource(randArray[number1]);
+
+            }
+        });
+
+
+
+
+
+
+
     }
 }
